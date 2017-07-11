b6845bd6bb96fa2ce45cc8f21d5faa74cfd6ca5d
mdaisuke <daisuke.mori9@gmail.com>
2017-07-12 08:45:30 +0900

    expr

--- tmp.s
+++ tmp.s
@@ -1,8 +1,8 @@
-	.data
-.mydata:
-	.string "abc"
-	.text
-	.global stringfn
-stringfn:
-	lea .mydata(%rip), %rax
+.text
+	.global intfn
+intfn:
+	mov $1, %rax
+	add $2, %rax
+	add $3, %rax
+	add $4, %rax
 	ret
